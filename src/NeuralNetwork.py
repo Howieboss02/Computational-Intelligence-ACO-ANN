4110aa0b0583f0c4be6c1a36ba6f93966ce38f00
Stanisław Howard <S.M.Howard@student.tudelft.nl>
2023-02-27 11:06:37 +0100

    added dev and assignment files

---
+++ src/NeuralNetwork.py
@@ -0,0 +1,10 @@
+# Note: you are free to organize your code in the way you find most convenient.
+# However, make sure that when your main notebook is run, it executes the steps indicated in the assignment.
+
+class Perceptron:
+    def __init__(self):
+        pass
+
+class ANN:
+    def __init__(self):
+        pass
