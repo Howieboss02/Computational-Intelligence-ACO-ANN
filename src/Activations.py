6355a2c0837bd698425fb384e3a59631542832c1
Stanislaw Howard <S.M.Howard@student.tudelft.nl>
2023-03-10 00:43:43 +0100

    activation function refactor

--- src/Activations.py
+++ src/Activations.py
@@ -1,40 +1,39 @@
 import numpy as np
 
-class Activation:
-    def __init__(self, alpha: float = 0, beta: float = 0):
-        '''
-        alpha - slope of the line for z < 0 for LReLU
-        beta - slope for the line for z >= 0 for LReLU
-        '''
-        self.alpha = alpha
-        self.beta = beta
+def step(z):
+    ''' Step activation function for output layer
+    z - input
+    '''
+    return np.where(z >= 0, 1, 0)
 
-    def softmax(self, z):
-        ''' Softmax activation function for output layer
-        z - input
-        '''
-        # subtract max value to get rid of dividing by a large numbers
-        # e_z = np.exp(z - np.max(z))
-        # return e_z / e_z.sum(axis=-1, keepdims=True)
-        # r = z.copy()
-        # r = r.squeeze()
-        r = z
-        exp_x = np.exp(r - np.max(r))  # subtract the maximum value for numerical stability
-        return exp_x / np.sum(exp_x)
+def softmax(z):
+    ''' Softmax activation function for output layer
+    z - input
+    '''
 
-    def sigmoid(self, z):
-        return 1 / (1 + np.exp(-z))
+    r = z
+    exp_x = np.exp(r - np.max(r))  # subtract the maximum value for numerical stability
+    return exp_x / np.sum(exp_x)
 
-    def LReLU(self, z):
-        ''' LReLU activation function for hidden layer
-        z - input
-        '''
-        np.where(z < 0, self.alpha * z, self.beta * z)
-        return z
+def sigmoid(z, derivative=False):
+    ''' Sigmoid activation function for hidden layer
+    z - input
+    '''
+    res = 1 / (1 + np.exp(-z))
+    if not derivative:
+        return res
+    else:
+        return res * (1 - res)
+     
+
+def LReLU(z, derivative=False):
+    ''' LReLU activation function for hidden layer
+    z - input
+    '''
+    alpha = 0.01
+    beta = 1
+    if not derivative:
+        np.where(z >= 0, beta * z, alpha * z)
+    else:
+        np.where(z >= 0, beta, alpha)
     
-    def LReLU_derivative(self, z):
-        ''' Derivative of LReLU activation function for hidden layer
-        z - input
-        '''
-        np.where(z < 0, self.alpha, self.beta)
-        return z
