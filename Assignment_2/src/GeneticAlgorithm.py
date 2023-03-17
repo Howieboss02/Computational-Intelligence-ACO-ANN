440e7675186a3c59a824c42e37bc6826a149eaba
Maksym Ziemlewski <M.Ziemlewski@student.tudelft.nl>
2023-03-17 16:51:30 +0100

    population creation

--- Assignment_2/src/GeneticAlgorithm.py
+++ Assignment_2/src/GeneticAlgorithm.py
@@ -1,5 +1,6 @@
 import random
 from TSPData import TSPData
+import numpy as np
 
 # TSP problem solver using genetic algorithms.
 class GeneticAlgorithm:
@@ -16,3 +17,35 @@ class GeneticAlgorithm:
     # @return the optimized product sequence.
     def solve_tsp(self, tsp_data):
         return []
+
+
+class Chromosome:
+    def __init__(self):
+        self.products = []
+        self.score = 0
+
+    def fitness_function(self, tsp_data):
+        route_length = 0
+        for product in range(len(self.products) - 1):
+            route_length += tsp_data.get_distances[self.products[product]][self.products[product + 1]]
+            # add distances to route_length
+
+    def create_chromosome(self, num_of_products):
+        products = np.arrange(num_of_products)
+        self.products = np.shuffle(products)
+
+
+class Population:
+    def __init__(self, mutation_prob, crossover_prob):
+        self.mutation_prob = mutation_prob
+        self.crossover_prob = crossover_prob
+        self.chromosomes = []
+        self.fitness_sum = 0
+
+    def create_population(self, pop_size, num_of_products):
+        for i in range(pop_size):
+            self.chromosomes.append(Chromosome().create_chromosome(num_of_products))
+
+
+
+
