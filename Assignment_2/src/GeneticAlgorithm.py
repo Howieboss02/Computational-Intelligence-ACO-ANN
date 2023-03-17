f4da810fe9e4c65b10661b30fd7e9a1dc67dc455
Maksym Ziemlewski <M.Ziemlewski@student.tudelft.nl>
2023-03-17 17:47:06 +0100

    fitness function

--- Assignment_2/src/GeneticAlgorithm.py
+++ Assignment_2/src/GeneticAlgorithm.py
@@ -24,27 +24,36 @@ class Chromosome:
         self.products = []
         self.score = 0
 
-    def fitness_function(self, tsp_data):
+    def fitness_function(self, distances_matrix):
         route_length = 0
         for product in range(len(self.products) - 1):
-            route_length += tsp_data.get_distances[self.products[product]][self.products[product + 1]]
-            # add distances to route_length
+            route_length += distances_matrix[self.products[product]][self.products[product + 1]]
+        self.score = route_length
+        return route_length
 
     def create_chromosome(self, num_of_products):
-        products = np.arrange(num_of_products)
+        products = np.arange(num_of_products)
         self.products = np.shuffle(products)
 
 
 class Population:
-    def __init__(self, mutation_prob, crossover_prob):
+    def __init__(self, mutation_prob, crossover_prob, tsp_data):
         self.mutation_prob = mutation_prob
         self.crossover_prob = crossover_prob
         self.chromosomes = []
         self.fitness_sum = 0
+        self.tsp_data = tsp_data
 
-    def create_population(self, pop_size, num_of_products):
+    def create_random_population(self, pop_size, num_of_products):
         for i in range(pop_size):
             self.chromosomes.append(Chromosome().create_chromosome(num_of_products))
+
+    def calculate_population_fitness(self):
+        fitness = 0
+        for chromosome in self.chromosomes:
+            fitness += chromosome.fitness_function(self.tsp_data.get_distances())
+        self.fitness_sum = fitness
+        return fitness
 
 
 
