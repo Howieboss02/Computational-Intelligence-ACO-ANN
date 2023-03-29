5f39f58d76545c8c1ae09fc7881f7f0325cad7d5
Stanisław Howard <S.M.Howard@student.tudelft.nl>
2023-03-29 14:48:33 +0200

    added tempalte for assignment 3

---
+++ Assignment_3/src/MyQLearning.py
@@ -0,0 +1,6 @@
+from QLearning import QLearning
+
+class MyQLearning(QLearning):
+    def update_q(self, state, action, r, state_next, possible_actions, alfa, gamma):
+        # TODO Auto-generated method stub
+        return
