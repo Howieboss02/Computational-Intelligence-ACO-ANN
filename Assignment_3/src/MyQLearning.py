bd954628be9e6bd96a44b224f36072d7d1d11956
Jakub Kontak <J.Kontak@student.tudelft.nl>
2023-04-02 20:19:34 +0200

    Implement basis for Q-Learning
    Implements 1.1, 1.2 ,1.3, 1.4, 1.5 and 1.6. However, there must be some bug there since plots for easy maze doesn't look correct

--- Assignment_3/src/MyQLearning.py
+++ Assignment_3/src/MyQLearning.py
@@ -1,6 +1,22 @@
 from QLearning import QLearning
+import numpy as np
 
 class MyQLearning(QLearning):
-    def update_q(self, state, action, r, state_next, possible_actions, alfa, gamma):
-        # TODO Auto-generated method stub
+    # This class extends QLearning
+    def update_q(self, state, action, r, state_next, possible_actions, alfa=0.7, gamma=0.9):
+        """
+        Use bellman formula from the assignment and update the q using self.set_q
+        :param state:
+        :param action:
+        :param r:
+        :param state_next:
+        :param possible_actions:
+        :param alfa:
+        :param gamma:
+        :return:
+        """
+        old_q = self.get_q(state, action)
+        best_q = np.max(self.get_action_values(state_next, possible_actions))
+        new_value = old_q + alfa * (r + gamma * best_q - old_q)
+        self.set_q(state, action, new_value)
         return
