df30760f31e43b1dc162f839d13d7283cabc9e2c
Jakub Kontak <J.Kontak@student.tudelft.nl>
2023-04-03 16:08:20 +0200

    Clean up the code

--- Assignment_3/src/MyQLearning.py
+++ Assignment_3/src/MyQLearning.py
@@ -5,15 +5,15 @@ class MyQLearning(QLearning):
     # This class extends QLearning
     def update_q(self, state, action, r, state_next, possible_actions, alfa=0.7, gamma=0.9):
         """
-        Use bellman formula from the assignment and update the q using self.set_q
-        :param state:
-        :param action:
-        :param r:
-        :param state_next:
-        :param possible_actions:
-        :param alfa:
-        :param gamma:
-        :return:
+        Use bellman formula from the assignment description and updates the Q matrix using self.set_q
+        :param state:       the state that we are in
+        :param action:      action taken in the above state
+        :param r:           reward for moving into 'state_next'
+        :param state_next:  state after we apply 'action' on 'state'
+        :param possible_actions:    list of possible action from the 'state_next'
+        :param alfa:        learning rate
+        :param gamma:       discount factor
+        :return:            nothing
         """
         old_q = self.get_q(state, action)
         best_q = np.max(self.get_action_values(state_next, possible_actions))
